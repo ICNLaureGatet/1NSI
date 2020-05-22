a2970c43ad57fb4fb2f56800a2f04ba6766dd573
ICNLaureGatet <remideniaud@gmail.com>
2020-05-22 22:23:45 +0200

    Add files via upload

---
+++ pageTest.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<!-- En-tête de la page -->
+        <meta charset="utf-8" />			<!-- C'est l'encodage de mon site -->
+		<title>Heure</title>		<!--C'est le titre de la page (écrit en haut de l'onglet du navigateur) -->
+    </head>
+
+
+	<body>
+		<p>Bonjour, il est : <?php echo date('H:i:s');?></p>
+	</body>
+</html>
