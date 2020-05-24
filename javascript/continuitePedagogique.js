1f40b2fba94a8f0c65a166ef09e0fe9321102fa1
ICNLaureGatet <remideniaud@gmail.com>
2020-05-24 23:32:16 +0200

    Update continuitePedagogique.js

--- javascript/continuitePedagogique.js
+++ javascript/continuitePedagogique.js
@@ -1,10 +1,11 @@
-let listeChap = ["reseaux", "IHMWeb", "systExplo", "CaptActio", "Algorithmique", "GenumSI"];
+let listeChap = ["reseaux", "IHMWeb", "systExplo", "CaptActio", "Algorithmique", "ClientServeur", "GenumSI"];
 let listeSousPartie = [
     ["Filius", "coursReseaux"],
     ["HTML", "CSS", "Javascript"],
     ["infosGen", "commandes"],
     ["CapteursArduino", "Arduino", "EntreesSorties"],
     ["RechercheDicho", "Algoknn", "AlgoGloutons"],
+    ["RequeteHTTP", "Formulaire"],
     []
 ]
 let contenuDiv = {} //j'en fais un objet plutôt qu'une liste
